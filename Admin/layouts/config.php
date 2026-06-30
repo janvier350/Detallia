f52a2277f4b777422a52ff38057bf45ae6b31f9d
Janvier Varas <janviervaras@gmail.com>
2026-06-30 10:17:13 -0500

    Primera versión - Detallia funcionando

---
+++ Admin/layouts/config.php
@@ -0,0 +1,21 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'buadnetc_detallia_user');
+define('DB_PASSWORD', '~zFr@W$anxq1#y;1');
+define('DB_NAME', 'buadnetc_detallia');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$gmailid = ''; // YOUR gmail email
+$gmailpassword = ''; // YOUR gmail password
+$gmailusername = ''; // YOUR gmail User name
+
+?>
